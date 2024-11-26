e673ca5807f0027013e700eb2d6d5f1d17b248ec
Gustavo <gustavomagalhaes3234@gmail.com>
2024-11-26 15:17:21 -0300

    Criando componente de definir meta da página Goals

---
+++ frontEnd/src/components/goalsContent/GoalsContent.tsx
@@ -0,0 +1,19 @@
+import "./GoalsContent.css"
+
+export function GoalsContent() {
+    return (
+        <section className="contentGoals">
+            <div className="GoalsDivs">
+                <section className="topGoals">
+                    <p>Here you can create a weekly question goal, enter the desired number of questions!</p>
+                    <input className="inputNumber"></input>
+                </section>
+
+                <section className="downGoals">
+                    <div className="percentGoals"></div>
+                    <p>COMPLETION RATE</p>
+                </section>
+            </div>
+        </section>
+    )
+}
--- frontEnd/src/pages/goalsPage/GoalsPage.tsx
+++ frontEnd/src/pages/goalsPage/GoalsPage.tsx
@@ -1,17 +1,20 @@
 import "./GoalsPage.css"
 import Navibar from "../../components/navibar/Navibar"
 import Header from "../../components/header/Header"
+import { GoalsContent } from "../../components/goalsContent/GoalsContent"
 
-export function GoalsPage(){
-    return(
+
+export function GoalsPage() {
+    return (
         <div className="contentTotal">
 
-            <Navibar/>
+            <Navibar />
 
             <section className="mainContent">
 
-                <Header/>
-                
+                <Header />
+                <GoalsContent/>
+
             </section>
 
         </div>
